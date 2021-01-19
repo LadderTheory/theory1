dd05eaa6cd24ec1c345d8da9b8cec4a359cb641f
Ladder Theory <brenton.ferullo@me.com>
2021-01-19 10:07:33 -0500

    added raildex.html and route to it

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/tyler', function(req, res) {
   res.sendFile(path.join(__dirname + '/tyler.html'));
 });
 
+app.get('/raildex', function(req, res) {
+  res.sendFile(path.join(__dirname + '/raildex.html'));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
