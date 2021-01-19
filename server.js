8d811f6b199a265a67cf04b73a30f43c0e38f5c0
Ladder Theory <brenton.ferullo@me.com>
2021-01-19 09:12:29 -0500

    added tyler.html; routed "/" to "/tyler"

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 80
+const path = require('path');
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('thank you tyler!')
+  res.redirect("../tyler");
 })
+
+app.get('/tyler', function(req, res) {
+  res.sendFile(path.join(__dirname + '/tyler.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
